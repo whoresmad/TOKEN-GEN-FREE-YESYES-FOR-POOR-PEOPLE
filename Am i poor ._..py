5f9076b8651ec74fe0e3693cecdede2b7baf1b18
Ratsuyo#0001 <94296856+user2bad@users.noreply.github.com>
2022-05-15 17:22:29 +0200

    Add files via upload

---
+++ Am i poor ._..py
@@ -0,0 +1,72 @@
+from multiprocessing.sharedctypes import Value
+from optparse import Option
+from pystyle import Colors, Colorate
+from pystyle import Write
+import random
+import os
+os.system("title Am i poor? ┃ Github: user2bad")
+
+os.system("cls")
+def menu():
+    niggerpoor
+
+niggerpoor = Write.Print(f"""
+▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒░░░░░░░░░░▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+▒▒▒▒▒▒▒▒▒▒▒▒▒░░░░░░░░░░░░░░░░░░░▒▒▒▒▒▒▒▒▒▒▒▒
+▒▒▒▒▒▒▒▒▒▒░░░░░░░░░░░░░░░░░░░░░░░░░▒▒▒▒▒▒▒▒▒
+▒▒▒▒▒▒▒▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▒▒▒▒▒▒▒
+▒▒▒▒▒▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▄░░▒▒▒▒▒
+▒▒▒▒▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░██▌░░▒▒▒▒
+▒▒▒▒░░░░░░░░░░░░░░░░░░░░░░░░░░░▄▄███▀░░░░▒▒▒
+▒▒▒░░░░░░░░░░░░░░░░░░░░░░░░░░░█████░▄█░░░░▒▒
+▒▒░░░░░░░░░░░░░░░░░░░░░░░░░░▄████████▀░░░░▒▒
+▒▒░░░░░░░░░░░░░░░░░░░░░░░░▄█████████░░░░░░░▒
+▒░░░░░░░░░░░░░░░░░░░░░░░░░░▄███████▌░░░░░░░▒
+▒░░░░░░░░░░░░░░░░░░░░░░░░▄█████████░░░░░░░░▒
+▒░░░░░░░░░░░░░░░░░░░░░▄███████████▌░░░░░░░░▒
+▒░░░░░░░░░░░░░░░▄▄▄▄██████████████▌░░░░░░░░▒
+▒░░░░░░░░░░░▄▄███████████████████▌░░░░░░░░░▒
+▒░░░░░░░░░▄██████████████████████▌░░░░░░░░░▒
+▒░░░░░░░░████████████████████████░░░░░░░░░░▒
+▒█░░░░░▐██████████▌░▀▀███████████░░░░░░░░░░▒
+▐██░░░▄██████████▌░░░░░░░░░▀██▐█▌░░░░░░░░░▒▒
+▒██████░█████████░░░░░░░░░░░▐█▐█▌░░░░░░░░░▒▒
+▒▒▀▀▀▀░░░██████▀░░░░░░░░░░░░▐█▐█▌░░░░░░░░▒▒▒
+▒▒▒▒▒░░░░▐█████▌░░░░░░░░░░░░▐█▐█▌░░░░░░░▒▒▒▒
+▒▒▒▒▒▒░░░░███▀██░░░░░░░░░░░░░█░█▌░░░░░░▒▒▒▒▒
+▒▒▒▒▒▒▒▒░▐██░░░██░░░░░░░░▄▄████████▄▒▒▒▒▒▒▒▒
+▒▒▒▒▒▒▒▒▒██▌░░░░█▄░░░░░░▄███████████████████
+▒▒▒▒▒▒▒▒▒▐██▒▒░░░██▄▄███████████████████████
+▒▒▒▒▒▒▒▒▒▒▐██▒▒▄████████████████████████████
+▒▒▒▒▒▒▒▒▒▒▄▄████████████████████████████████
+████████████████████████████████████████████
+\n\n╔═════════════════════════════╗\n║[1] Am i poor?               ║
+║[2] Check if someone is poor ║
+║[3] Credits                  ║
+║[0] Quit                     ║        \n╚═════════════════════════════╝""", Colors.red_to_purple, interval=0.000001)
+
+menu()
+option = int(Write.Input(f"\n╔══[Choose your option]\n╚══► ", Colors.red_to_purple, interval=0.000001))
+while option != 0:
+    if option ==1:
+        Write.Input(f"\n╔══[Am i poor?]\n╚══► no", Colors.red_to_purple, interval=0.000001)
+        pass
+    elif option ==2:
+
+        niggername = Write.Input(f"\n╔══[Enter Name]\n╚══► ", Colors.red_to_purple, interval=0.000001)
+        Write.Print(f'\n╔══[Is {niggername} poor] \n╚══► yes', Colors.red_to_purple, interval=0.000001)
+
+        pass
+    elif option ==3:
+        Write.Print(f"Github: user2bad\nSolo.to: soreness\nDiscord Main: Ratsuyo#0001\nDiscord Contact/Alt: Ratsuyo#0002\nServer: discord.gg/termsofservice", Colors.red_to_purple, interval=0.000001)
+        
+        pass
+    else:
+        Write.Print(f"Invalid, niggerr don't troll")
+
+    print()
+    menu()
+    option = int(Write.Input(f"\n╔══[Choose your option]\n╚══► ", Colors.red_to_purple, interval=0.000001))
+
+print("I hope you found som poorons/had fun.. Cya!", Colors.red_to_purple, interval=0.000001)
