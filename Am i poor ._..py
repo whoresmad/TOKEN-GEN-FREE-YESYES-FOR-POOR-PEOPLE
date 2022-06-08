82cded450b3d0b612c7be3be589299e1c59dbb4b
! ◝漢Brooklyn漢◜ <94296856+GotRipped@users.noreply.github.com>
2022-06-09 01:04:02 +0200

    Update Am i poor ._..py

--- Am i poor ._..py
+++ Am i poor ._..py
@@ -4,7 +4,7 @@ from pystyle import Colors, Colorate
 from pystyle import Write
 import random
 import os
-os.system("title Am i poor? ┃ Github: user2bad")
+os.system("title Am i poor? ┃ Github: GotRipped")
 
 os.system("cls")
 def menu():
@@ -59,7 +59,7 @@ while option != 0:
 
         pass
     elif option ==3:
-        Write.Print(f"Github: user2bad\nSolo.to: soreness\nDiscord Main: Ratsuyo#0001\nDiscord Contact/Alt: Ratsuyo#0002\nServer: discord.gg/termsofservice", Colors.red_to_purple, interval=0.000001)
+        Write.Print(f"Github: GotRipped\clippy.link: sex\nDiscord Main: brook#6666\nDiscord Contact/Alt: Ripped#0001\nServer: discord.gg/termsofservice", Colors.red_to_purple, interval=0.000001)
         
         pass
     else:
