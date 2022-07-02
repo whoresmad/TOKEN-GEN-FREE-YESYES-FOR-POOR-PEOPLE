e5f90210f0e59166fae3c0505367a4955b8b9942
漢weebstar漢 <94296856+lemmebe@users.noreply.github.com>
2022-07-02 12:23:14 +0200

    Update Am i poor ._..py

--- Am i poor ._..py
+++ Am i poor ._..py
@@ -4,7 +4,7 @@ from pystyle import Colors, Colorate
 from pystyle import Write
 import random
 import os
-os.system("title Am i poor? ┃ By: Gingest")
+os.system("title Am i poor? ┃ By: weebstar")
 
 os.system("cls")
 def menu():
@@ -59,7 +59,7 @@ while option != 0:
 
         pass
     elif option ==3:
-        Write.Print(f"Github: lemmebe\clippy.link: sex\nDiscord Gιngesτ#0001\nServer: discord.gg/vcc", Colors.red_to_purple, interval=0.000001)
+        Write.Print(f"Github: lemmebe\clippy.link: sex\nDiscord weebstar#0001\nServer: discord.gg/vcc", Colors.red_to_purple, interval=0.000001)
         
         pass
     else:
